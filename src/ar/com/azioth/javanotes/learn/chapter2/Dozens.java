cf99748cd756afb4536de8a0ee6e35ede740e9ed
Sebastian Sandri <ssandri@velocitypartnets.net>
2015-09-15 17:15:54 -0300

    Added Exercise 2.5: Dozens

--- src/ar/com/azioth/javanotes/learn/chapter2/Dozens.java
+++ src/ar/com/azioth/javanotes/learn/chapter2/Dozens.java
@@ -1,10 +1,22 @@
 package ar.com.azioth.javanotes.learn.chapter2;
 
+import ar.com.azioth.javanotes.utils.TextIO;
+
 public class Dozens {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int eggsInput;
+		int gross;
+		int dozens;
+		int leftOvers;
+		
+		System.out.print("How many eggs do you have:    ");
+		eggsInput = TextIO.getInt();
+		gross = eggsInput / 144;
+		dozens = (eggsInput % 144) / 12;
+		leftOvers = eggsInput % 12;
+		
+		System.out.println("Your number of eggs is " + gross + " gross, " + dozens + " dozen and " + leftOvers+ " left over.");
 	}
-
+	
 }
